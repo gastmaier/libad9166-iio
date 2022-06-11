a36442e32da469f26c6279abd891ace4df8fb19b
Cosmin Tanislav <demonsingur@gmail.com>
2022-06-11 19:50:55 +0300

    Implement calibration methods

--- ad9166.c
+++ ad9166.c
@@ -16,7 +16,10 @@
 
 #include <errno.h>
 #include <iio.h>
+#include <math.h>
 #include <stdio.h>
+#include <stddef.h>
+#include <string.h>
 #ifdef _WIN32
 #include <windows.h>
 #else
@@ -24,4 +27,150 @@
 #include <time.h>
 #endif
 
+#define MAX_ATTR_NAME		128
+#define MAX_CALIB_LENGTH	32
+#define ARRAY_DELIM " [,]"
 
+static int parse_array(const char *value, double **arr, size_t *len)
+{
+	char *value_dup = strdup(value);
+
+	*arr = calloc(MAX_CALIB_LENGTH, sizeof(double));
+	*len = 0;
+
+	char *tk = strtok(value_dup, ARRAY_DELIM);
+	char *endptr;
+	while (tk) {
+		double n;
+
+		printf("%s\n", tk);
+		n = strtod(tk, &endptr);
+
+		if (*endptr) {
+			fprintf(stderr, "Illegal character: %c\n", *endptr);
+			return -EINVAL;
+		} else if (*len == MAX_CALIB_LENGTH) {
+			fprintf(stderr, "Too many calibration values, max: %u\n",
+				MAX_CALIB_LENGTH);
+			return -EINVAL;
+		}
+
+		(*arr)[(*len)++] = n;
+
+		tk = strtok(NULL, ARRAY_DELIM);
+	}
+
+	free(value_dup);
+
+	return 0;
+}
+
+int ad9166_context_find_calibration_data(struct iio_context *ctx,
+					 const char *name,
+					 struct ad9166_calibration_data **data)
+{
+	char freq_attr_name[MAX_ATTR_NAME];
+	char offset_attr_name[MAX_ATTR_NAME];
+	char gain_attr_name[MAX_ATTR_NAME];
+	int ret;
+
+	snprintf(freq_attr_name, sizeof(freq_attr_name), "%s%s", name, "_freq");
+	snprintf(offset_attr_name, sizeof(offset_attr_name), "%s%s", name, "_offset");
+	snprintf(gain_attr_name, sizeof(gain_attr_name), "%s%s", name, "_gain");
+
+	const char *freq_attr = iio_context_get_attr_value(ctx, freq_attr_name);
+	const char *offset_attr = iio_context_get_attr_value(ctx, offset_attr_name);
+	const char *gain_attr = iio_context_get_attr_value(ctx, gain_attr_name);
+
+	size_t freqs_len, offsets_len, gains_len;
+	double *freqs, *offsets, *gains;
+
+	ret = parse_array(freq_attr, &freqs, &freqs_len);
+	if (ret)
+		return ret;
+
+	ret = parse_array(offset_attr, &offsets, &offsets_len);
+	if (ret)
+		return ret;
+
+	ret = parse_array(gain_attr, &gains, &gains_len);
+	if (ret)
+		return ret;
+
+	if (freqs_len != offsets_len || offsets_len != gains_len)
+		return -EINVAL;
+
+	*data = malloc(sizeof(**data));
+	(*data)->freqs = freqs;
+	(*data)->offsets = offsets;
+	(*data)->gains = gains;
+	(*data)->len = freqs_len;
+
+	return 0;
+}
+
+int ad9166_channel_set_freq(struct iio_channel *ch,
+			    unsigned long long int freq)
+{
+	int ret;
+
+	ret = iio_channel_attr_write_longlong(ch, "nco_frequency", freq);
+	if (ret < 0)
+		return ret;
+
+	return 0;
+}
+
+int ad9166_device_set_amplitude(struct iio_device *dev,
+				unsigned long long int amplitude)
+{
+	int ret;
+
+	unsigned int reg_amplitude_dec = powl(10, amplitude / 20) * 32767;
+	ret = iio_device_reg_write(dev, 0x14E, reg_amplitude_dec >> 8);
+	if (ret)
+		return ret;
+
+	ret = iio_device_reg_write(dev, 0x14F, reg_amplitude_dec & 0xFF);
+	if (ret)
+		return ret;
+
+	return 0;
+}
+
+int ad9166_device_set_iofs(struct iio_device *dev,
+			   struct ad9166_calibration_data *data,
+			   unsigned long long int freq)
+{
+	int ret;
+
+	if (data->len < 1)
+		return -EINVAL;
+
+	unsigned int iofs;
+
+	if (freq < 1000000000)
+		iofs = data->offsets[0];
+
+	for (unsigned int i = 0; i < data->len; i++) {
+		if (i == data->len - 1
+		    || freq >= data->freqs[i] && freq < data->freqs[i + 1]) {
+			iofs = data->offsets[i] +  data->gains[i]
+			       * (freq - data->freqs[i]);
+			break;
+		}
+	}
+
+	if (iofs > 1023)
+		iofs = 1023;
+
+	ret = iio_device_reg_write(dev, 0x42, iofs >> 2);
+	if (ret)
+		return ret;
+
+	ret = iio_device_reg_write(dev, 0x41, iofs & 0x3);
+	if (ret)
+		return ret;
+
+	return 0;
+}
--- ad9166.h
+++ ad9166.h
@@ -18,6 +18,8 @@
 #ifndef __AD9166_H__
 #define __AD9166_H__
 
+#include <stddef.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -44,12 +46,33 @@ extern "C" {
 
 struct iio_context;
 struct iio_device;
+struct iio_channel;
+
+struct ad9166_calibration_data {
+	double *freqs;
+	double *offsets;
+	double *gains;
+	size_t len;
+};
 
 /* ---------------------------------------------------------------------------*/
 /* ------------------------- Top-level functions -----------------------------*/
 /** @defgroup TopLevel Top-level functions
  * @{ */
 
+__api int ad9166_context_find_calibration_data(struct iio_context *ctx,
+					       const char *name,
+					       struct ad9166_calibration_data **data);
+
+__api int ad9166_channel_set_freq(struct iio_channel *ch,
+				  unsigned long long int freq);
+
+__api int ad9166_device_set_amplitude(struct iio_device *dev,
+				      unsigned long long int amplitude);
+
+__api int ad9166_device_set_iofs(struct iio_device *dev,
+				 struct ad9166_calibration_data *data,
+				 unsigned long long int freq);
 
 /** @} */
 
