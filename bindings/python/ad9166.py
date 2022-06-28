322a521bc0117a1d45323ead1b4c6f6e16214272
Antoniu Miclaus <antoniu.miclaus@analog.com>
2022-06-28 14:03:16 +0300

    ad9166: add python bindings
    Add function implementation for the python bindings of libad9166
    
    Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

--- bindings/python/ad9166.py
+++ bindings/python/ad9166.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 # SPDX-License-Identifier: LGPL-2.1-or-later
 # Copyright (C) 2021 Analog Devices, Inc.
-# Author: Travis F. Collins <travis.collins@analog.com>
+# Author: Antoniu Miclaus <antoniu.miclaus@analog.com>
 
 from typing import List
 import math
 
 import iio
-from iio import _ContextPtr, _DevicePtr
+from iio import _ContextPtr, _DevicePtr, _ChannelPtr
 
 # Imports from package ctypes are not grouped
 # pylint: disable=ungrouped-imports
@@ -20,10 +20,13 @@ from ctypes import (
     c_ubyte,
     c_double,
     c_int,
+    c_char,
     c_longlong,
+    c_ulonglong,
     c_short,
     c_uint,
     c_ulong,
+    c_char_p
     CDLL as _cdll,
     POINTER as _POINTER,
     Structure,
@@ -55,3 +58,90 @@ else:
     _libad9166 = "ad9166"
 
 _lib = _cdll(find_library(_libad9166), use_errno=True, use_last_error=True)
+
+class CalibrationParameters(Structure):
+     _fields_ = [
+        ("Freqs", _POINTER(c_double)),
+        ("Offsets", _POINTER(c_double)),
+        ("Gains", _POINTER(c_double)),
+        ("Len", c_double),
+    ]
+
+_ad9166_context_find_calibration_data = _lib.ad9166_context_find_calibration_data
+_ad9166_context_find_calibration_data.restype = c_int
+_ad9166_context_find_calibration_data.argtypes = (_ContextPtr, _POINTER(c_char), _POINTER(_POINTER(CalibrationParameters)))
+_ad9166_context_find_calibration_data.errcheck = _check_negative
+
+_ad9166_device_set_amplitude = _lib.ad9166_device_set_amplitude
+_ad9166_device_set_amplitude.restype = c_int
+_ad9166_device_set_amplitude.argtypes = (_DevicePtr, c_longlong)
+_ad9166_device_set_amplitude.errcheck = _check_negative
+
+_ad9166_channel_set_frequency = _lib.ad9166_channel_set_freq
+_ad9166_channel_set_frequency.restype = c_int
+_ad9166_channel_set_frequency.argtypes = (_ChannelPtr, c_ulonglong)
+_ad9166_channel_set_frequency.errcheck = _check_negative
+
+_ad9166_device_set_iofs = _lib.ad9166_device_set_iofs
+_ad9166_device_set_iofs.restype = c_int
+_ad9166_device_set_iofs.argtypes = (_DevicePtr, _POINTER(CalibrationParameters), c_ulonglong)
+_ad9166_device_set_iofs.errcheck = _check_negative
+
+def find_calibration_data(ctx: iio.Context, name: str, data: CalibrationParameters):
+    """Calibration configuration.
+    :param: iio.Context dev: IIO Context of AD9166 driver
+    :param str name: Desired name of the device
+    :param CalibrationParameters data: Calibration parameters
+    """
+
+    ret = _ad9166_context_find_calibration_data(ctx._context, c_char_p(str(name).encode('utf-8')), byref(data))
+
+    if ret != 0:
+        raise Exception(f"Loading Calibration data failed. ERROR: {ret}")
+
+    return ret
+
+def set_amplitude(dev: iio.Device, amplitude: int):
+    """Amplitude configuration.
+    :param: iio.Device dev: IIO Device object of AD9166 driver
+    :param int amplitude: Desired amplitude between 0dB and -91dB
+    """
+    if math.trunc(amplitude) != amplitude:
+        raise Exception("amplitude must be an integer")
+
+    ret = _ad9166_device_set_amplitude(dev._device, c_longlong(int(amplitude)))
+
+    if ret != 0:
+        raise Exception(f"Setting amplitude failed. ERROR: {ret}")
+
+    return ret
+
+def set_frequency(ch: iio.Channel, frequency: int):
+    """Frequency configuration.
+    :param: iio.Channel dev: IIO Channel object of AD9166 driver
+    :param int frequency: Desired frequency
+    """
+    if math.trunc(frequency) != frequency:
+        raise Exception("frequency must be an integer")
+
+    ret = _ad9166_channel_set_frequency(ch._channel, c_ulonglong(int(frequency)))
+
+    if ret != 0:
+        raise Exception(f"Setting frequency failed. ERROR: {ret}")
+
+    return ret
+
+def device_set_iofs(dev: iio.Device, data: CalibrationParameters, frequency: int):
+    """IOFS configuration.
+    :param: iio.Device dev: IIO Device object of AD9166 driver
+    :param CalibrationParameters data: Calibration parameters
+    :param int frequency: Desired frequency
+    """
+    if math.trunc(frequency) != frequency:
+        raise Exception("frequency must be an integer")
+
+    ret = _ad9166_device_set_iofs(dev._device, data, c_ulonglong(int(frequency)))
+    if ret != 0:
+        raise Exception("Setting IOFS failed. ERROR: {ret}")
+
+    return ret
--- bindings/python/test/test_ad9166.py
+++ bindings/python/test/test_ad9166.py
@@ -5,4 +5,17 @@ import ad9166
 import os
 import pathlib
 
+ctx = iio.Context("local:")
+dev = ctx.find_device("ad9166")
+ch = dev.find_channel("altvoltage0", True)
 
+ad9166.set_amplitude(dev, -10)
+ad9166.set_frequency(ch, 3000000000)
+
+data = ad9166._POINTER(ad9166.CalibrationParameters)()
+
+ad9166.find_calibration_data(ctx, "cn0511", data)
+
+print(data.contents.Freqs.contents)
+
+ad9166.device_set_iofs(dev, data, 3000000000)
