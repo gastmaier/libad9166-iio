1e5c04112230a86c630f39e38fe05ee66c394a94
Cosmin Tanislav <demonsingur@gmail.com>
2022-07-01 10:14:47 +0300

    Make python test pass without hardware

--- bindings/python/test/test_ad9166.py
+++ bindings/python/test/test_ad9166.py
@@ -1,17 +1,22 @@
+import pytest
+
 import iio
 import ad9166
 
-ctx = iio.Context("local:")
-dev = ctx.find_device("ad9166")
-ch = dev.find_channel("altvoltage0", True)
+@pytest.mark.iio_hardware(["ad9166"])
+def test_set_calibrated_amplitude_frequency(iio_uri):
+    ctx = iio.Context(iio_uri)
+    dev = ctx.find_device("ad9166")
 
-ad9166.set_amplitude(dev, -10)
-ad9166.set_frequency(ch, 3000000000)
+    ch = dev.find_channel("altvoltage0", True)
 
-data = ad9166._POINTER(ad9166.CalibrationParameters)()
+    ad9166.set_amplitude(dev, -10)
+    ad9166.set_frequency(ch, 3000000000)
 
-ad9166.find_calibration_data(ctx, "cn0511", data)
+    data = ad9166._POINTER(ad9166.CalibrationParameters)()
 
-print(data.contents.Freqs.contents)
+    ad9166.find_calibration_data(ctx, "cn0511", data)
 
-ad9166.device_set_iofs(dev, data, 3000000000)
+    print(data.contents.Freqs.contents)
+
+    ad9166.device_set_iofs(dev, data, 3000000000)
